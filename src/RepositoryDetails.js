d51125e6766a582544d5055ddbf98cf91ca79603
mervAyz <merveayaz9898@gmail.com>
2023-10-16 21:46:31 +0300

    ADDED: MaterialUI library & RepositoryDetails.js & RepositoryDetails.css & UserRepositories.css - EDITED: UserRepositories.js

---
+++ src/RepositoryDetails.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Button, Modal, Backdrop, Fade } from '@mui/material';
+import './RepositoryDetails.css';
+
+function RepositoryDetails({ repo}) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleShow} className="button-details"> {/* Apply the button-details class here */}
+        Details
+      </Button>
+      <Modal
+        open={showModal}
+        onClose={handleClose}
+        closeAfterTransition
+        className="modal"
+      >
+        <Fade in={showModal}>
+          <div className="modal-paper">
+            <h2 className="modal-title">Repository Details</h2>
+            <p>Watchers: {repo.watchers.totalCount}</p>
+            <p>Forks: {repo.forks.totalCount}</p>
+            <p>Stars: {repo.stargazers.totalCount}</p>
+            <p>Languages:</p>
+            <ul>
+              {repo.languages.edges.map((edge, index) => (
+                <li key={index}>{edge.node.name}</li>
+              ))}
+            </ul>
+            <Button variant="contained" color="secondary" onClick={handleClose} className="modal-footer"> {/* Apply the modal-footer class here */}
+              Close
+            </Button>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+export default RepositoryDetails;
--- src/UserRepositories.js
+++ src/UserRepositories.js
@@ -2,7 +2,8 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { gql } from 'graphql-tag';
 import { useAppState } from './AppStateContext';
-import './UserRepositories.css'
+import './UserRepositories.css';
+import RepositoryDetails from './RepositoryDetails';
 
 const GET_USER_REPOSITORIES = gql`
   query GetUserRepositories($username: String!, $first: Int, $afterCursor: String) {
@@ -16,6 +17,22 @@ const GET_USER_REPOSITORIES = gql`
           cursor
           node {
             name
+            watchers {
+              totalCount
+            }
+            forks {
+              totalCount
+            }
+            stargazers {
+              totalCount
+            }
+            languages(first: 10) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
           }
         }
       }
@@ -27,21 +44,23 @@ function UserRepositories() {
   const { state, dispatch } = useAppState();
   const [getRepositories, { loading, error, data }] = useLazyQuery(GET_USER_REPOSITORIES);
 
+
   const handleSearch = () => {
     dispatch({ type: 'SET_REPOSITORIES', payload: [] });
     getRepositories({ variables: { username: state.username, first: 10, afterCursor: null } });
   };
 
   const handleLoadMore = () => {
-      const afterCursor = data.user.repositories.pageInfo.endCursor;
-      getRepositories({ variables: { username: state.username, first: 10, afterCursor } });
-    
+    const afterCursor = data.user.repositories.pageInfo.endCursor;
+    getRepositories({ variables: { username: state.username, first: 10, afterCursor } });
   };
+
+
 
   useEffect(() => {
     if (data) {
       const newRepos = data.user.repositories.edges.map((edge) => edge.node);
-        dispatch({ type: 'SET_REPOSITORIES', payload: [...state.repositories, ...newRepos] });
+      dispatch({ type: 'SET_REPOSITORIES', payload: [...state.repositories, ...newRepos] });
     }
   }, [data]);
 
@@ -60,11 +79,16 @@ function UserRepositories() {
       {state.repositories.length > 0 && (
         <div>
           <h2>Repositories for {state.username}:</h2>
-          <ol>
+          <ul>
             {state.repositories.map((repo, index) => (
-              <li key={index}>{repo.name}</li>
+              <li key={index}>
+                {repo.name}
+                <RepositoryDetails
+                  repo={repo}
+                />
+              </li>
             ))}
-          </ol>
+          </ul>
           {data.user.repositories.pageInfo.hasNextPage && (
             <button onClick={handleLoadMore}>Load More</button>
           )}
